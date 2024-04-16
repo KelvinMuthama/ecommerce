fe2ee410d3e3c9ca5c340e3b4691486ee94159b5
kelvinmuthama <kelvinkamau387@gmail.com>
2024-04-16 16:58:25 +0300

    Style: Add styles for navbar. Make it dynamic.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,31 @@
-import { UserButton } from "@clerk/nextjs";
+import { auth, UserButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/MainNav";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+import StoreSwitcher from "./StoreSwitcher";
+
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
-        <MainNav className="mx-6"/>
+        <div>
+          <StoreSwitcher items={stores} />
+        </div>
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
